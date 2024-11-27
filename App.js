25d7b820adda9ff516128945970a5becac01f631
tejas-vivek <tejas.helwatkar@gmail.com>
2024-11-26 19:28:32 -0500

    Make separate components for Input and Display

--- App.js
+++ App.js
@@ -1,43 +1,44 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import GoalItem from "./component/GoalItem";
+import GoalInput from "./component/GoalInput";
 
 export default function App() {
+  
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goals!"/>
-        <Button title="Add Goal"/>
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
-        <Text>List of goals</Text>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
-      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
+  appContainer: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 16
-  },
-  inputContainer:{
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    margin: 8,
-    padding: 8
+    paddingHorizontal: 16,
   },
   goalsContainer: {
     flex: 5,
-  }
+  },
 });
---
+++ component/GoalItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const GoalItem = (props) => {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{props.text}</Text>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+  },
+  goalText: {
+    color: "white",
+  },
+});
